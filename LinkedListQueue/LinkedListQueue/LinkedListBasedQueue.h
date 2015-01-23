8439caee26cc7e362ed087f8b57fd845a1a5dda5
Younghoo Kim <younghoo88@gmail.com>
2015-01-23 18:26:19 +0900

    LinkedList Based Queue 코드 추가

--- LinkedListQueue/LinkedListQueue/LinkedListBasedQueue.h
+++ LinkedListQueue/LinkedListQueue/LinkedListBasedQueue.h
@@ -10,5 +10,29 @@
 #define __LinkedListQueue__LinkedListBasedQueue__
 
 #include <stdio.h>
+#include <stdlib.h>
+#define QUE_LEN 100
+#define TRUE 1
+#define FALSE 0
+
+typedef int Data;
+
+typedef struct _node {
+  Data data;
+  struct _node * next;
+} Node;
+
+typedef struct _lQueue {
+  Node * front;
+  Node * rear;
+} LQueue;
+
+typedef LQueue Queue;
+
+void QueueInit(Queue * pq);
+int QIsEmpty(Queue * pq);
+void Enqueue(Queue * pq, Data data);
+Data Dequeue(Queue * pq);
+Data QPeek(Queue * pq);
 
 #endif /* defined(__LinkedListQueue__LinkedListBasedQueue__) */
--- LinkedListQueue/LinkedListQueue/main.c
+++ LinkedListQueue/LinkedListQueue/main.c
@@ -7,9 +7,23 @@
 //
 
 #include <stdio.h>
+#include "LinkedListBasedQueue.h"
 
 int main(int argc, const char * argv[]) {
-  // insert code here...
-  printf("Hello, World!\n");
-    return 0;
+  Queue queue;
+  QueueInit(&queue);
+  
+  // 데이터 넣기
+  Enqueue(&queue, 1);
+  Enqueue(&queue, 2);
+  Enqueue(&queue, 3);
+  Enqueue(&queue, 4);
+  Enqueue(&queue, 5);
+  
+  // 데이터 꺼내기
+  while (!QIsEmpty(&queue)) {
+    printf("%d ", Dequeue(&queue));
+  }
+  
+  return 0;
 }
